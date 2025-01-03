a52795d78d1e06a051dd5cb25b1542d795887d18
Alexa <aqua5834@gmail.com>
2025-01-03 01:32:06 -0500

    adding basic JS

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+const initialCards = [
+    {name:"Winter Whiteout", 
+        link:"https://unsplash.com/photos/a-black-and-white-photo-of-a-snowy-park-hqKEIg8SdLY"}
+    {name:"Flower in the stars", 
+        link:"https://unsplash.com/photos/the-night-sky-is-filled-with-stars-and-a-yellow-flower-KEINy-0eDuk"}
+    {name:"Algea on the rocks", 
+        link:"https://unsplash.com/photos/a-rocky-beach-with-green-algae-growing-in-the-water-QRVY1g2URDA"}
+    {name:"Waterfall in the mountains", 
+        link:"https://unsplash.com/photos/a-large-waterfall-is-in-the-middle-of-a-mountain-AhM0yBuW1_I"}
+    {name:"Misty days", 
+        link:"https://unsplash.com/photos/a-village-in-the-mountains-covered-in-snow-k-uOL1vBUxM"}
+    {name:"A sky full of stars", 
+        link:"https://unsplash.com/photos/a-night-sky-with-stars-and-a-silhouette-of-a-person-HofZ13d2H94"}];
